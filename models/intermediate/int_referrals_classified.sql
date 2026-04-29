61ddb5469f46ad98fd85a13d2a2b40b9dfe039d9
davidhitman <107502959+davidhitman@users.noreply.github.com>
2026-04-29 10:21:30 +0200

    Refactor SQL view with updated joins and case statements

--- models/intermediate/int_referrals_classified.sql
+++ models/intermediate/int_referrals_classified.sql
@@ -1,56 +1,29 @@
 {{ config(materialized='view') }}
 
-with consultations as (
-
-    select *
-    from {{ ref('stg_consultations_fixed') }}
-
-),
-
-outcomes as (
-
-    select *
-    from {{ ref('stg_clinical_outcomes') }}
-
-),
-
-intake as (
-
-    select *
-    from {{ ref('stg_consultation_requests') }}
-
-),
-
-joined as (
+with joined as (
 
     select
         c.consultation_id,
 
-        coalesce(o.referral_issued, 0) as doctor_referral,
-        coalesce(i.referral_requested, 0) as patient_requested
+        ifNull(o.referral_issued, 0) as doctor_referral,
+        ifNull(i.referral_requested, 0) as patient_requested
 
-    from consultations c
-    left join outcomes o
+    from {{ ref('stg_consultations_fixed') }} c
+    left join {{ ref('stg_clinical_outcomes') }} o
         on c.consultation_id = o.consultation_id
-    left join intake i
+    left join {{ ref('stg_consultation_requests') }} i
         on c.consultation_id = i.consultation_id
-
-),
-
-classified as (
-
-    select
-        consultation_id,
-
-        case
-            when doctor_referral = 1 and patient_requested = 1 then 'both'
-            when doctor_referral = 1 then 'doctor_referral'
-            when patient_requested = 1 then 'patient_requested_only'
-            else 'no_referral'
-        end as referral_type
-
-    from joined
 
 )
 
-select * from classified
+select
+    consultation_id,
+
+    case
+        when doctor_referral = 1 and patient_requested = 1 then 'both'
+        when doctor_referral = 1 then 'doctor_referral'
+        when patient_requested = 1 then 'patient_requested_only'
+        else 'no_referral'
+    end as referral_type
+
+from joined
